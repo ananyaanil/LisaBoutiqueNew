f00d285c94c53791e036b5776310caca96e113ab
ananyaanil <ananya.anil@smail.rasmussen.edu>
2017-08-29 16:12:05 -0500

    Update opendb.php

--- www/opendb.php
+++ www/opendb.php
@@ -1,8 +1,16 @@
 <?php
-// Create connection
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:aanilserver.database.windows.net,1433; Database = Lisdatabase", "aanil12", "ashley98R");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "aanil12@aanilserver", "pwd" => "ashley98R", "Database" => "Lisdatabase", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:aanilserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
