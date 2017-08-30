96fb76869a3e5fbe45a16ab4406b084e8a540eeb
ananyaanil <ananya.anil@smail.rasmussen.edu>
2017-08-30 16:53:15 -0500

    Update find.php

--- www/find.php
+++ www/find.php
@@ -1,28 +1,11 @@
-<?php // REGISTER USER 
-include_once('opendb.php'); 
+?php // REGISTER USER include_once('opendb.php'); 
 $username = ($_POST['uname']); $password_1 = ($_POST['psw']); 
-$tsql="SELECT * FROM login WHERE username='$username'and password='$password_1'"; 
+$tsql="SELECT * FROM login WHERE username='$username'"; 
 $result=sqlsrv_query($conn,$sql); $count=sqlsrv_num_rows($result); 
-// If result matched $username and $password, table row must be 1 row 
-// if($count==1) 
-// {
-    $row = sqlsrv_fetch_array($result); 
-    if ($password_1)) == $row['password'])
-    { 
-      session_register("username"); 
-      session_register("password"); 
-      <?php header('Location: home.html');?>
-      echo "Login Successful";
-      <html> <body> <p>LOGIN SUCCESSFUL</p> </body> </html>
+// If result matched $username and $password, table row must be 1 row if($count==1){ $row = sqlsrv_fetch_array($result); 
+if ($password_1)) == $row['password']){ session_register("username"); session_register("password"); 
+<?php header('Location: home.html');?>
+echo "Login Successful"; <html> <body> <p>LOGIN SUCCESSFUL</p> </body> </html>
 
-      exit;
-      return true; } 
-    else
-    {
-      <?php header('Location: login.php');?> 
-       echo "Wrong Username or Password";
-      <html> <head> <title>My Title</title> </head> <body> <p>LOGIN FAILED</p> </body> </html> 
-     
-      return false; } 
-// }
-?>
+</script>"; exit; return true; } else { echo "Wrong Username or Password";
+<html> <head> <title>My Title</title> </head> <body> <p>LOGIN FAILED</p> </body> </html> header(“Location: login.php”); return false; } ?
