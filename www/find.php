5c20406757c8e993d8f2743cb5f08a1adf74b104
ananyaanil <ananya.anil@smail.rasmussen.edu>
2017-08-30 16:28:39 -0500

    Update find.php

--- www/find.php
+++ www/find.php
@@ -2,7 +2,8 @@
 $username = ($_POST['uname']); $password_1 = ($_POST['psw']); 
 $tsql="SELECT * FROM login WHERE username='$username'and password='$password_1'"; 
 $result=sqlsrv_query($conn,$sql); $count=sqlsrv_num_rows($result); 
-// If result matched $username and $password, table row must be 1 row if($count==1){ $row = sqlsrv_fetch_array($result); 
+// If result matched $username and $password, table row must be 1 row 
+if($count==1){ $row = sqlsrv_fetch_array($result); 
 if ($password_1)) == $row['password']){ session_register("username"); session_register("password"); 
 <?php header('Location: home.html');?>
 echo "Login Successful"; <html> <body> <p>LOGIN SUCCESSFUL</p> </body> </html>
