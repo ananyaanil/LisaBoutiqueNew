047a4c07e441417d67f34ff234012002e48e2cb5
ananyaanil <ananya.anil@smail.rasmussen.edu>
2017-08-30 16:11:50 -0500

    Update find.php

--- www/find.php
+++ www/find.php
@@ -5,6 +5,6 @@ $result=sqlsrv_query($conn,$sql); $count=sqlsrv_num_rows($result);
 // If result matched $username and $password, table row must be 1 row if($count==1){ $row = sqlsrv_fetch_array($result); 
 if ($password_1)) == $row['password']){ session_register("username"); session_register("password"); 
 echo "Login Successful"; <html> <body> <p>LOGIN SUCCESSFUL</p> </body> </html>
-echo "<script type='text/javascript'> document.location = 'page1.php'; 
+echo "<script type='text/javascript'> document.location = 'home.html'; 
 </script>"; exit; return true; } else { echo "Wrong Username or Password";
 <html> <head> <title>My Title</title> </head> <body> <p>LOGIN FAILED</p> </body> </html> header(“Location: login.php”); return false; } ?
