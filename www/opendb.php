7c3d0c7d4e8ef47965bb6b4cb2065da89884e7bc
ananyaanil <ananya.anil@smail.rasmussen.edu>
2017-09-01 16:28:42 -0500

    Update opendb.php

--- www/opendb.php
+++ www/opendb.php
@@ -5,7 +5,7 @@ $port=3306;
 $socket="";
 $user="mysqldbuser@lisbotique-mysqldbserver";
 $password="ashley98R";
-$dbname="lisas' account";
+$dbname="accounts";
 
 $conn = new mysqli($host, $user, $password, $dbname, $port, $socket) 
 or die ('Could not connect to the database server' . mysqli_connect_error());
