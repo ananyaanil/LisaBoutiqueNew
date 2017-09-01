4a6fd6697edabc94113ec3078c4dcc9d203d8973
ananyaanil <ananya.anil@smail.rasmussen.edu>
2017-08-31 23:28:36 -0500

    Update find.php

--- www/find.php
+++ www/find.php
@@ -1,35 +1,24 @@
-?php /* // REGISTER USER include_once('opendb.php'); 
-$username = ($_POST['uname']); $password_1 = ($_POST['psw']); 
-$tsql="Select * from login where username='$username'and password ='$password_1 '"
-$result=sqlsrv_query($conn,$tsql);
-$count=sqlsrv_num_rows($result); 
-// If result matched $username and $password, table row must be 1 row if($count==1){ $row = sqlsrv_fetch_array($result); 
-if ($password_1)) == $row['password']){ session_register("username"); session_register("password"); 
-<?php header('Location: home.html');?>
-echo "Login Successful"; <html> <body> <p>LOGIN SUCCESSFUL</p> </body> </html>
-
-</script>"; exit; return true; } else { echo "Wrong Username or Password";
-<html> <head> <title>My Title</title> </head> <body> <p>LOGIN FAILED</p> </body> </html> header(“Location: login.php”); return false; } 
-*/ ?>
 <?php
-//include_once('opendb.php'); 
+include_once('opendb.php'); 
 $username = ($_POST['uname']); 
 $password_1 = ($_POST['psw']); 
-$tsql="Select * from login where username='$username' and password = '$password_1'";
-$result=sqlsrv_query($conn,$tsql);$count=sqlsrv_num_rows($result); 
-// If result matched $username and $password, table row must be 1 row
- if($count==1){ 
-     $row = sqlsrv_fetch_array($result); 
-     
- session_register("username"); 
-session_register("password"); 
- //header('Location: home.html');
-echo "
-     <html> <body> <p>LOGIN SUCCESSFUL</p> </body> </html> "}
-     else{
-     echo " <html> <body> <p>LOGIN NOT SUCCESSFUL</p> </body> </html>"
-     }
+$sql="Select * from lisas' account.login where username='$username' and password = '$password_1'";
+$result = mysqli_query($conn,$sql);
+$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+$active = $row['active'];
 
+$count = mysqli_num_rows($result);
+
+// If result matched $myusername and $mypassword, table row must be 1 row
+  
+if($count == 1) {
+    echo "success!";
+    session_start();
+    $_SESSION['loggedin'] = true;
+    $_SESSION['username'] = $username;
+    header("location: home.html");
+}else {
+    echo "Your Login Name or Password is invalid!";
+}
 ?>
-
 
