547e95d32c8093b146c320e6f2a9aa3b6e279c1d
ananyaanil <ananya.anil@smail.rasmussen.edu>
2017-09-01 16:47:12 -0500

    Update find.php

--- www/find.php
+++ www/find.php
@@ -19,6 +19,7 @@ if($count == 1) {
     header("location: home.html");
 }else {
     $error = "Your Login Name or Password is invalid!! $username";
+  header("location: home.html#section2");
 }
-echo $error;
+
 ?>
