7fa2b89a7f6dca06a1868d5a6804276b2968cf57
ananyaanil <ananya.anil@smail.rasmussen.edu>
2017-09-04 13:05:46 -0500

    Update logout.php

--- www/logout.php
+++ www/logout.php
@@ -1,4 +1,9 @@
 <html>
 <h1> Logout Successful </h1>
 <a href="home.html"> click here to Home page </a>
+  	<div data-role="footer"  style=" bottom:0;" data-theme="b">
+  <div class="logo"><img src="img/LOGO.png" width="200" height="200" alt=""/>
+		
+	  <h4>lisa's Boutique &copy; 2017</h4>
+	</div>
 </html>
